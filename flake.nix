bc871a93e6f81efcfc72c84ad7c009a6d9743079
Michael Stone <m@mstone.info>
2020-03-14 19:04:28 -0400

    vsc: app settings + go + rust

--- flake.nix
+++ flake.nix
@@ -10,12 +10,25 @@
       vscode = vscodium;
       vscodeExtensions = with vscode-extensions; [
         bbenoist.Nix
+        ms-vscode.Go
+      ] ++ vscode-utils.extensionsFromVscodeMarketplace [
+        {
+          name = "rust";
+          publisher = "rust-lang";
+          version = "0.7.0";
+          sha256 = "sha256-QPO5IA5mrYo6cn3hdTjmzhbRN/YU7G4yMspJ+dRBx5o=";
+        }
       ];
     };
 
     defaultPackage.x86_64-darwin = packages.x86_64-darwin.vsc;
 
-    defaultApp.x86_64-darwin = packages.x86_64-darwin.vsc;
+    apps.x86_64-darwin.vsc = {
+      type = "app";
+      program = "${self.packages.x86_64-darwin.vsc}/bin/codium";
+    };
+
+    defaultApp.x86_64-darwin = apps.x86_64-darwin.vsc;
 
   };
 }
