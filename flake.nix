ce80f97ae4b6fe8b8d39c25fc62e1aadd7aed498
Michael Stone <m@mstone.info>
2020-03-14 17:43:09 -0400

    vsc += nix

--- flake.nix
+++ flake.nix
@@ -1,12 +1,21 @@
 
 {
-  description = "A flake for building Hello World";
+  description = "A flake for building vscodium with appropriate extensions";
 
   edition = 201909;
 
-  outputs = { self, nixpkgs }: {
+  outputs = { self, nixpkgs }: rec {
 
-    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
+    packages.x86_64-darwin.vsc = with nixpkgs.legacyPackages.x86_64-darwin; vscode-with-extensions.override {
+      vscode = vscodium;
+      vscodeExtensions = with vscode-extensions; [
+        bbenoist.Nix
+      ];
+    };
+
+    defaultPackage.x86_64-darwin = packages.x86_64-darwin.vsc;
+
+    defaultApp.x86_64-darwin = packages.x86_64-darwin.vsc;
 
   };
 }
