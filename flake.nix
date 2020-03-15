d7fb23cef4e34ef13998b21049541fed5e10a438
Michael Stone <m@mstone.info>
2020-03-14 21:21:32 -0400

    vsc += rust 1.42.0

--- flake.nix
+++ flake.nix
@@ -4,9 +4,34 @@
 
   edition = 201909;
 
-  outputs = { self, nixpkgs }: rec {
+  inputs = {
 
-    packages.x86_64-darwin.vsc = with nixpkgs.legacyPackages.x86_64-darwin; vscode-with-extensions.override {
+    nixpkgs = {
+      uri = "nixpkgs";
+    };
+
+    moz_overlay_src = {
+      url = "git+https://github.com/mozilla/nixpkgs-mozilla";
+      flake = false;
+    };
+
+  };
+
+  outputs = { self, nixpkgs, moz_overlay_src }: let
+
+    moz_overlay = import moz_overlay_src;
+
+    nps = import nixpkgs { system = "x86_64-darwin"; overlays = [ moz_overlay ]; };
+
+    rust1 = with nps; (rustChannelOf {
+      channel = "stable";
+      date = "2020-03-12";
+      sha256 = "0pddwpkpwnihw37r8s92wamls8v0mgya67g9m8h6p5zwgh4il1z6";
+    }).rust.override {
+      targets = ["wasm32-unknown-unknown"];
+    };
+
+    vsc = with nps; vscode-with-extensions.override {
       vscode = vscodium;
       vscodeExtensions = with vscode-extensions; [
         bbenoist.Nix
@@ -18,6 +43,21 @@
           version = "0.7.0";
           sha256 = "sha256-QPO5IA5mrYo6cn3hdTjmzhbRN/YU7G4yMspJ+dRBx5o=";
         }
+      ];
+    };
+
+  in rec {
+
+    packages.x86_64-darwin.vsc = with nixpkgs.legacyPackages.x86_64-darwin; stdenv.mkDerivation {
+      pname = "vsc";
+      version = "1.0";
+      phases = [ "installPhase" ];
+      installPhase = ''
+        mkdir -p $out/bin
+        makeWrapper ${vsc}/bin/codium $out/bin/codium --prefix PATH : ${lib.makeBinPath [ go_1_13 rust1 ]}
+      '';
+      buildInputs = [
+        makeWrapper
       ];
     };
 
