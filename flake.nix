6d93a2f11e587636e070ed4e7e26f984630bd51b
Michael Stone <m@mstone.info>
2021-09-21 14:21:24 -0400

    vsc: build vscodium from source

--- flake.nix
+++ flake.nix
@@ -1,75 +1,140 @@
 
 {
-  description = "A flake for building vscodium with selected extensions";
+  description = "A flake for building codium with selected extensions";
 
   inputs.nixpkgs.url = "nixpkgs";
-  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  inputs.utils.url = "github:numtide/flake-utils";
+
+  inputs.codiumSrc.url = "github:VSCodium/vscodium";
+  inputs.codiumSrc.flake = false;
+
+  inputs.vscodeSrc.url = "github:microsoft/vscode";
+  inputs.vscodeSrc.flake = false;
+
   inputs.rust-overlay.url = "github:oxalica/rust-overlay";
-  inputs.rust-overlay.inputs.flake-utils.follows = "flake-utils";
+  inputs.rust-overlay.inputs.flake-utils.follows = "utils";
   inputs.rust-overlay.inputs.nixpkgs.follows = "nixpkgs";
+
   inputs.vscode-drawio.url = "https://github.com/hediet/vscode-drawio";
   inputs.vscode-drawio.flake = false;
   inputs.vscode-drawio.type = "git";
   inputs.vscode-drawio.submodules = true;
 
-  outputs = { self, nixpkgs, flake-utils, rust-overlay, vscode-drawio }: flake-utils.lib.simpleFlake {
-    inherit self nixpkgs;
-    name = "vsc";
+  outputs = { self, nixpkgs, utils, codiumSrc, vscodeSrc, rust-overlay, vscode-drawio }: let
+    name = "codium";
+  in utils.lib.simpleFlake {
+    inherit self nixpkgs name;
     preOverlays = [ rust-overlay.overlay ];
-    systems = [ "x86_64-linux" "x86_64-darwin" ];
+    systems = utils.lib.defaultSystems;
     config = {
       allowUnsupportedSystem = true;
     };
     overlay = final: prev: {
-      vsc = with final; rec {
-        env = [ rust-bin.stable.latest.rust asciidoctor wkhtmltopdf go_1_16 ];
+      codium = with final; rec {
+        # env = [ rust-bin.stable.latest.default asciidoctor wkhtmltopdf go_1_16 ];
+        env = [];
 
-        wrapper = vscode-with-extensions.override {
-          vscode = vscodium;
+        fakeSwVers = writeShellScriptBin "sw_vers" ''
+          echo 10.15
+        '';
+
+        hdiutilWrapper = writeShellScriptBin "hdiutil" ''
+          exec /usr/bin/hdiutil "$@"
+        '';
+
+        codiumFromSrc = stdenv.mkDerivation {
+          pname = "codium";
+          version = "1.59.1";
+          longName = "codium";
+          shortName = "codium";
+          executableName = "vscodium";
+          src = codiumSrc;
+          buildInputs = [ nodejs-14_x git cacert yarn python39 fakeSwVers jq ] ++ (with darwin.apple_sdk.frameworks; [ Security AppKit Cocoa ]);
+          nativeBuildInputs = [ darwin.cctools hdiutilWrapper ];
+          buildPhase = ''
+            #set -x
+            #set -euo pipefail
+            export GIT_SSL_CAINFO="${cacert}/etc/ssl/certs/ca-bundle.crt";
+            mkdir vscode; cp -a ${vscodeSrc}/. vscode/.
+            chmod -R u+rwX vscode
+            # remove postinstall commands that expect vscode/.git
+            head -n -2 vscode/build/npm/postinstall.js > js.tmp
+            mv js.tmp vscode/build/npm/postinstall.js
+            rm patches/custom-gallery.patch patches/fix-cors.patch
+            mkdir yarn
+            HOME=$(pwd)/yarn MS_COMMIT=${vscodeSrc.rev} SHOULD_BUILD=yes CI_BUILD=no OS_NAME=osx VSCODE_ARCH=arm64 . build.sh
+          '';
+          installPhase = ''
+            cp -a ./ $out/
+          '';
+        };
+
+        codiumSubset = stdenv.mkDerivation {
+          pname = "codium";
+          version = "1.59.1";
+          src = codiumFromSrc;
+          phases = [ "installPhase" ];
+          installPhase = ''
+            mkdir -p $out
+            cp -a $src/VSCode-darwin-arm64/VSCodium.app/. $out/.
+          '';
+        };
+
+        codiumGeneric = callPackage "${nixpkgs}/pkgs/applications/editors/vscode/generic.nix" rec {
+          sourceRoot = "";
+          version = "1.59.1";
+          pname = "vscodium";
+          executableName = "codium";
+          longName = "VSCodium";
+          shortName = "codium";
+          src = codiumSubset;
+          updateScript = "";
+          meta = {
+            description = ''
+              Open source source code editor developed by Microsoft for Windows,
+              Linux and macOS (VS Code without MS branding/telemetry/licensing),
+              built from source
+            '';
+            homepage = "https://github.com/VSCodium/vscodium";
+          };
+        };
+
+        hediet-vscode-drawio = stdenv.mkDerivation rec {
+          name = "vscode-extension-hediet.vscode-drawio";
+          vscodeExtUniqueId = "hediet.vscode-drawio";
+          version = "1.4.0-alpha.3";
+          src = vscode-drawio;
+          buildInputs = [ yarn nodePackages.webpack nodePackages.webpack-cli ];
+          buildPhase = ''
+            mkdir yarn && HOME=$(pwd)/yarn yarn install --immutable && yarn build
+          '';
+          installPrefix = "share/vscode/extensions/${vscodeExtUniqueId}";
+          installPhase = ''
+            mkdir -p "$out/$installPrefix/";
+            for f in CHANGELOG.md README.md dist docs drawio package.json; do
+              mv $f "$out/$installPrefix";
+            done
+          '';
+        };
+
+        codiumWithExtensions = vscode-with-extensions.override {
+          vscode = codiumGeneric;
           vscodeExtensions = with vscode-extensions; [
-            bbenoist.Nix
+            bbenoist.nix
             matklad.rust-analyzer
-          ] ++ vscode-utils.extensionsFromVscodeMarketplace [
-            {
-              name = "asciidoctor-vscode";
-              publisher = "joaompinto";
-              version = "2.7.13";
-              sha256 = "sha256-os4vsusgf6izymcvUAN+XCJFBuG0fzh+gIxabHgxjeI=";
-            }
-            {
-              name = "Go";
-              publisher = "golang";
-              version = "0.22.1";
-              sha256 = "sha256-VKwKS091nEP0f6i/Mx5/1Kw45mejhERU/F+4WN8Ia70=";
-            }
-          ] ++ [
-            (stdenv.mkDerivation rec {
-              name = "vscode-extension-hediet.vscode-drawio";
-              vscodeExtUniqueId = "hediet.vscode-drawio";
-              version = "1.4.0-alpha.3";
-              src = vscode-drawio;
-              buildInputs = [ yarn nodePackages.webpack nodePackages.webpack-cli ];
-              buildPhase = ''
-                mkdir yarn && HOME=$(pwd)/yarn yarn install --immutable && yarn build
-              '';
-              installPrefix = "share/vscode/extensions/${vscodeExtUniqueId}";
-              installPhase = ''
-                mkdir -p "$out/$installPrefix/";
-                for f in CHANGELOG.md README.md dist docs drawio package.json; do
-                  mv $f "$out/$installPrefix";
-                done
-              '';
-            })
+            golang.go
+            hediet-vscode-drawio
           ];
         };
 
-	codium = stdenv.mkDerivation {
+        codium = stdenv.mkDerivation {
           pname = "codium";
           version = "1.0";
           phases = ["installPhase"];
           installPhase = ''
             mkdir -p $out/bin;
-            makeWrapper ${wrapper}/bin/codium $out/bin/codium --prefix PATH : ${lib.makeBinPath env}
+            makeWrapper ${codiumWithExtensions}/bin/codium $out/bin/codium --prefix PATH : ${lib.makeBinPath env}
           '';
           buildInputs = [ makeWrapper ];
         };
